99a2a7148240e58d9181d0ffabcffa6f8b23a557
zmundo <zmundo36@gmail.com>
2018-04-20 19:49:49 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/about", function(req, res){
+  res.render("about");
+});
+
+
+app.listen('3000');
